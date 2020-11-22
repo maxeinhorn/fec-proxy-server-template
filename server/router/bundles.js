6f09bd2749b5b150ba63600190363da1487c3e7a
Max Einhorn <maxeinhorn@gmail.com>
2020-11-21 16:09:36 -0800

    Styling working--data still not pulling

--- server/router/bundles.js
+++ server/router/bundles.js
@@ -6,37 +6,37 @@ const {
 
 const router = Router();
 
-// router.use('/service1.js', createProxyMiddleware({
-//   target: service1.url,
-//   pathRewrite: {
-//     '^/bundles/service1.js': service1.bundle,
-//   },
-//   changeOrigin: true,
-// }));
+router.use('/service1.js', createProxyMiddleware({
+  target: service1.url,
+  pathRewrite: {
+    '^/bundles/service1.js': service1.bundle,
+  },
+  changeOrigin: true,
+}));
 
-// router.use('/style.css-Service1', createProxyMiddleware({
-//   target: service1.url,
-//   pathRewrite: {
-//     '^/bundles/style.css-service1': service1.style,
-//   },
-//   changeOrigin: true,
-// }));
+router.use('/style.css-Service1', createProxyMiddleware({
+  target: service1.url,
+  pathRewrite: {
+    '^/bundles/style.css-service1': service1.style,
+  },
+  changeOrigin: true,
+}));
 
-// router.use('/service2.js', createProxyMiddleware({
-//   target: service2.url,
-//   pathRewrite: {
-//     '^/bundles/service2.js': service2.bundle,
-//   },
-//   changeOrigin: true,
-// }));
+router.use('/service2.js', createProxyMiddleware({
+  target: service2.url,
+  pathRewrite: {
+    '^/bundles/service2.js': service2.bundle,
+  },
+  changeOrigin: true,
+}));
 
-// router.use('/style.css-Service2', createProxyMiddleware({
-//   target: service2.url,
-//   pathRewrite: {
-//     '^/bundles/style.css-service2': service2.style,
-//   },
-//   changeOrigin: true,
-// }));
+router.use('/style.css-Service2', createProxyMiddleware({
+  target: service2.url,
+  pathRewrite: {
+    '^/bundles/style.css-service2': service2.style,
+  },
+  changeOrigin: true,
+}));
 
 router.use('/service3.js', createProxyMiddleware({
   target: service3.url,
